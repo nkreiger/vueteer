54e7a4d40cbca1a4e7b1a3abdd98b768cd873e1c
Noah Kreiger <noahkreiger@Noahs-MacBook-Pro.local>
2020-05-09 15:29:05 -0400

    added ui integration

--- src/server/controllers/pptr/pptr.js
+++ src/server/controllers/pptr/pptr.js
@@ -2,8 +2,8 @@ const { pptr } = require('../../services');
 const delay = require('delay');
 
 const handleCmds = async (req, res) => {
+    console.log(req.body);
     const cmds = req.body.cmds;
-
     for (const c of cmds) {
         const f = pptr.services[c.type][c.cmd.value];
         await delay(500);
